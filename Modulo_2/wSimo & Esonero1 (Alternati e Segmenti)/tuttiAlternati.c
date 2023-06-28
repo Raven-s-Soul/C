bd3ec145c5d332d0a1dc51edc8226b085f8099be
Sario Tedesco <sariotedesco@gmail.com>
2023-06-28 18:02:41 +0200

    update

---
+++ Modulo_2/wSimo & Esonero1 (Alternati e Segmenti)/tuttiAlternati.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* funzione ricorsiva che verifica ... */
+int tuttiAlternati(int *seq, int lunghezza)
+{
+}
+
+/* funzione che conta ... */
+int quantiNumeri()
+{
+}
+
+/* funzione che legge ... */
+void leggiNumeri(int *sequenza, int lunghezza)
+{
+}
+
+/* funzione che scrive ... */
+void scriviNumeri(int *sequenza, int lunghezza)
+{
+}
+
+/* funzione principale */
+int main()
+{
+    int lunghezza; //	lunghezza dell'array
+    int scelta;    //	scelta dell'utente
+
+    /* acquisizione dati e lettura dell'array da input */
+    printf("Vuoi introdurre una nuova sequenza (premi 1) oppure recuperarla da file (premi 2)? ");
+    scanf("%d", &scelta);
+
+    /* introduzione sequenza da input */
+    if (scelta == 1)
+    {
+        /* determina la lunghezza della sequenza */
+        printf("Introduci la lunghezza della sequenza: ");
+        scanf("%d", &lunghezza);
+
+        /* crea l'array e leggine i valori degli elementi */
+        int array[lunghezza];
+        printf("Scrivi %d elementi \n", lunghezza);
+        for (int i = 0; i < lunghezza; i++)
+            scanf("%d", &array[i]);
+
+        /* invoca la funzione ricorsiva e stampa il risultato */
+        if (tuttiAlternati(array, lunghezza))
+            printf(" ... \n");
+        else
+            printf(" ... \n");
+
+        /* salva la sequenza su file */
+        scriviNumeri(array, lunghezza);
+    }
+    /* recupero sequenza da file */
+    else
+    {
+        /* determina la lunghezza della sequenza */
+        lunghezza = quantiNumeri();
+
+        /* crea l'array e leggine i valori degli elementi da file */
+        int array[lunghezza];
+        leggiNumeri(array, lunghezza);
+        printf("Ecco la sequenza recuperata\n");
+        for (int i = 0; i < lunghezza; i++)
+            printf("%d ", array[i]);
+        printf("\n");
+
+        /* invoca la funzione ricorsiva e stampa il risultato */
+        if (tuttiAlternati(array, lunghezza))
+            printf(" ... \n");
+        else
+            printf(" ... \n");
+
+        /* salva la sequenza su file */
+        scriviNumeri(array, lunghezza);
+    }
+}
