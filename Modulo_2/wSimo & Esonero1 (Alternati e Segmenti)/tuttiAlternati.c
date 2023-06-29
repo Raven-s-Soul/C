d733bc0ec6fa60cd5beea6ccfb1b6857f7e27712
Sario Tedesco <sariotedesco@gmail.com>
2023-06-29 16:37:04 +0200

    tuttiAlternati.c

--- Modulo_2/wSimo & Esonero1 (Alternati e Segmenti)/tuttiAlternati.c
+++ Modulo_2/wSimo & Esonero1 (Alternati e Segmenti)/tuttiAlternati.c
@@ -1,24 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define fname "interi.dat"
+
 /* funzione ricorsiva che verifica ... */
 int tuttiAlternati(int *seq, int lunghezza)
 {
+    int res = 1;
+    if (lunghezza < 2)
+    {
+        res = 1;
+    }
+    else
+    {
+        if (!(((seq[0] * seq[1]) < 0) && tuttiAlternati(seq + 1, lunghezza - 1)))
+            res = 0;
+    }
+    return res;
 }
 
 /* funzione che conta ... */
 int quantiNumeri()
 {
+    FILE *fp = fopen(fname, "rb");
+    int count = 0;
+    if (!fp)
+    {
+        printf("\nErrore apertura file\n");
+    }
+    else
+    {
+        int buffer;
+        while (fread(&buffer, sizeof(int), 1, fp) > 0)
+        {
+            count++;
+        }
+    }
+    fclose(fp);
+    // printf("count : %d\n", count);
+    return count;
 }
 
 /* funzione che legge ... */
 void leggiNumeri(int *sequenza, int lunghezza)
 {
+    FILE *fp = fopen(fname, "rb");
+    if (fp == NULL)
+    {
+        printf("\nErrore scrittura file\n");
+    }
+    else
+    {
+        for (int i = 0; i < lunghezza; i++)
+        {
+            fread(&sequenza[i], sizeof(int), 1, fp);
+        }
+    }
+    fclose(fp);
 }
 
 /* funzione che scrive ... */
 void scriviNumeri(int *sequenza, int lunghezza)
 {
+    FILE *fp = fopen(fname, "wb");
+    if (fp == NULL)
+    {
+        printf("\nErrore scrittura file\n");
+    }
+    else
+    {
+        for (int i = 0; i < lunghezza; i++)
+        {
+            fwrite(&sequenza[i], sizeof(int), 1, fp);
+        }
+    }
+    fclose(fp);
 }
 
 /* funzione principale */
@@ -46,9 +102,9 @@ int main()
 
         /* invoca la funzione ricorsiva e stampa il risultato */
         if (tuttiAlternati(array, lunghezza))
-            printf(" ... \n");
+            printf(" Positivo \n");
         else
-            printf(" ... \n");
+            printf(" Negativo \n");
 
         /* salva la sequenza su file */
         scriviNumeri(array, lunghezza);
@@ -69,9 +125,9 @@ int main()
 
         /* invoca la funzione ricorsiva e stampa il risultato */
         if (tuttiAlternati(array, lunghezza))
-            printf(" ... \n");
+            printf(" Positivo \n");
         else
-            printf(" ... \n");
+            printf(" Negativo \n");
 
         /* salva la sequenza su file */
         scriviNumeri(array, lunghezza);
